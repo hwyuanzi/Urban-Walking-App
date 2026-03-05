81dbd778396f3bb8f657024b8dae3d1e17353c9d
hwyuanzi <hy2821@nyu.edu>
2026-03-05 10:34:01 -0500

    Refactor app.py logic, update env.example

--- app.py
+++ app.py
@@ -27,8 +27,8 @@ class User(UserMixin):
     def __init__(self, user_data):
         self.id = str(user_data['_id'])
         self.username = user_data['username']
-        self.following = user_data['following']
-        self.likes = user_data['likes']
+        self.following = user_data.get('following', [])
+        self.likes = user_data.get('likes', [])
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -40,25 +40,33 @@ def load_user(user_id):
         return User(user_data)
     return None
 
+# Helper function to handle like toggling
+def toggle_like(trail_id, user):
+    trail = trails_collection.find_one({"_id": ObjectId(trail_id)})
+    if not trail:
+        return
+
+    if user.id in trail['likes']:
+        trail['likes'].remove(user.id)
+        user.likes.remove(trail_id)
+    else:
+        trail['likes'].append(user.id)
+        user.likes.append(trail_id)
+    
+    users_collection.update_one({'_id': ObjectId(user.id)}, {"$set": {"likes": user.likes}})
+    trails_collection.update_one({'_id': ObjectId(trail_id)}, {"$set": {"likes": trail['likes']}})
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
+        if not current_user.is_authenticated:
+            flash('Please login to like trails.', 'error')
+            return redirect(url_for('login'))
+
         liked = request.form.get('like')
         if liked:
-            trails = trails_collection.find_one({"_id" : ObjectId(liked)})
-            if current_user.id in trails['likes']:
-                trails['likes'].remove(current_user.id)
-                current_user.likes.remove(liked)
-                print("remove")
-            else:
-                trails['likes'].append(current_user.id)
-                current_user.likes.append(liked)
-                print("add")
-            print(current_user.likes)
-            users_collection.update_one({'_id': ObjectId(current_user.id)}, {"$set": {"likes": current_user.likes}})
-            trails_collection.update_one({'_id': ObjectId(liked)}, {"$set": {"likes": trails['likes']}})
+            toggle_like(liked, current_user)
             return redirect(url_for('index'))
     query1 = request.args.get('q1')
     query2 = request.args.get('q2')
@@ -79,7 +87,7 @@ def index():
         ]
     if query2:
         escaped_query = re.escape(query2)
-        mongo_query["creator_username"] = escaped_query
+        mongo_query["creator_username"] = {"$regex": escaped_query, "$options": "i"}
     
     # 2. Handle filters
     if filter_neighborhood:
@@ -89,7 +97,7 @@ def index():
     if filter_duration:
         mongo_query['duration'] = filter_duration
     checked = False
-    if filter_following:
+    if filter_following and current_user.is_authenticated:
         mongo_query['created_by'] = {"$in": current_user.following}
         checked = True
 
@@ -168,29 +176,31 @@ def show_user(user_id):
 
 @app.route('/trail/info/<trail_id>', methods=['GET', 'POST'])
 def trail_info(trail_id):
-    trails = trails_collection.find_one({"_id" : ObjectId(trail_id)})
+    try:
+        trail = trails_collection.find_one({"_id" : ObjectId(trail_id)})
+    except InvalidId:
+        flash('Invalid trail ID.', 'error')
+        return redirect(url_for('index'))
+
+    if not trail:
+        flash('Trail not found.', 'error')
+        return redirect(url_for('index'))
+
     if request.method == 'POST':
+        if not current_user.is_authenticated:
+            flash('Please login to comment or like.', 'error')
+            return redirect(url_for('login'))
+            
         liked = request.form.get('like')
         if liked:
-            trails = trails_collection.find_one({"_id" : ObjectId(liked)})
-            if current_user.id in trails['likes']:
-                trails['likes'].remove(current_user.id)
-                current_user.likes.remove(liked)
-                print("remove")
-            else:
-                trails['likes'].append(current_user.id)
-                current_user.likes.append(liked)
-                print("add")
-            print(current_user.likes)
-            users_collection.update_one({'_id': ObjectId(current_user.id)}, {"$set": {"likes": current_user.likes}})
-            trails_collection.update_one({'_id': ObjectId(liked)}, {"$set": {"likes": trails['likes']}})
+            toggle_like(liked, current_user)
             return redirect(url_for('trail_info', trail_id=trail_id))
         comment = {str(current_user.username) : request.form.get('comment')}
-        trails["comments"].append(comment)
-        newComments = {'comments' : trails["comments"]}
+        trail["comments"].append(comment)
+        newComments = {'comments' : trail["comments"]}
         trails_collection.update_one({'_id': ObjectId(trail_id)}, {'$set': newComments})
         flash('Comment posted successfully!', 'success')
-    return render_template('trail_info.html', trail=trails)
+    return render_template('trail_info.html', trail=trail)
 
 @app.route('/trail/<trail_id>/edit', methods=['GET', 'POST'])
 @login_required
